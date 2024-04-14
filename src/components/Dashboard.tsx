41b339e44ef2690d390418c8e7bf489a2250736f
ankan321 <adalui260@gmail.com>
2024-04-14 07:17:34 +0530

    first commit

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -39,13 +39,12 @@ const Dashboard = ({ subscriptionPlan }: PageProps) => {
       <div className="mt-8 flex flex-col items-start justify-between gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0">
         <h1 className="mb-3 font-bold text-5xl text-gray-900">My Files</h1>
 
-        <Link href="https://inspiron-summary.vercel.app/">
-
-          <Button>Summary Web</Button>
-
-        </Link>
-        <UploadButton isSubscribed={subscriptionPlan.isSubscribed} />
-
+        <div className="flex items-center gap-4">
+          <Link href="https://inspiron-summary.vercel.app/">
+            <Button>Summary Web</Button>
+          </Link>
+          <UploadButton isSubscribed={subscriptionPlan.isSubscribed} />
+        </div>
       </div>
 
       {/* display all user files */}
